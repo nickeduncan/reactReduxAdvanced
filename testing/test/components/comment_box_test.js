51f7b9963115add197060417b1b8558e2fc45366
Nick Duncan <nickeduncan@gmail.com>
2017-05-13 21:48:04 -0400

    began tests for text input and simulated input

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -18,4 +18,18 @@ describe('CommentBox', () => {
   it('has a button', () => {
     expect(component.find('button')).to.exist;
   });
+
+  describe('entering some text', () => {
+    beforeEach(() => {
+      component.find('textarea').simulate('change', 'new comment');
+    });
+
+    it('shows that text in the text area', () => {
+
+    });
+
+    it('when submitted, clears the input', () => {
+
+    });
+  })
 });
