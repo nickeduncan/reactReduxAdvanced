6def0999e95cb9fc5045aa8e26d8db5af762f681
Nick Duncan <nickeduncan@gmail.com>
2017-05-13 20:37:34 -0400

    added test to ensure app displays correct text

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -2,10 +2,18 @@ import { renderComponent, expect } from '../test_helper';
 import App from '../../src/components/app';
 
 // use 'describe' to group together similar tests
-describe('App')
+describe('App', () => {
 
-// use 'it' to test a single attribute of a target
-it('shows the correct test')
+  // use 'it' to test a single attribute of a target
+  it('shows the correct text', () => {
 
-// use 'expect' to make an 'assertion' about a target
-expect
+    // create an instance of app
+    const component = renderComponent(App);
+
+    // use 'expect' to make an 'assertion' about a target
+    expect(component).to.contain('React simple start');
+
+  });
+
+
+});
