525491b1648cf00424c38b089b7338e38c18e575
Nick Duncan <nickeduncan@gmail.com>
2017-05-13 21:38:26 -0400

    render commentbox in app and wrote test

--- testing/test/components/app_test.js
+++ testing/test/components/app_test.js
@@ -3,17 +3,14 @@ import App from '../../src/components/app';
 
 // use 'describe' to group together similar tests
 describe('App', () => {
+  let component;
 
-  // use 'it' to test a single attribute of a target
-  it('shows the correct text', () => {
-
-    // create an instance of app
-    const component = renderComponent(App);
-
-    // use 'expect' to make an 'assertion' about a target
-    expect(component).to.contain('React simple starter');
-
+  beforeEach(() => {
+    component = renderComponent(App);
   });
 
+  it('shows a comment box', () => {
+    expect(component.find('.comment-box')).to.exist;
+  });
 
 });
