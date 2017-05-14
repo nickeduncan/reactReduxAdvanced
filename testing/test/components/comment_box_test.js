91646a8a436ac27ac3e4ef20c1812b8161a04acc
Nick Duncan <nickeduncan@gmail.com>
2017-05-13 21:56:18 -0400

    wrote test for change in input

--- testing/test/components/comment_box_test.js
+++ testing/test/components/comment_box_test.js
@@ -25,7 +25,7 @@ describe('CommentBox', () => {
     });
 
     it('shows that text in the text area', () => {
-
+      expect(component.find('textarea')).to.have.value('new comment');
     });
 
     it('when submitted, clears the input', () => {
